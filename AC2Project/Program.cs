61fe63a702b208d2778cfa9d197038bb8375d729
vfsouza <vini.f.souza@hotmail.com>
2022-04-26 00:29:05 -0300

    Update 1

--- AC2Project/Program.cs
+++ AC2Project/Program.cs
@@ -6,59 +6,8 @@ namespace AC2Project {
 
 
 		public static Dictionary<string, int> mnemonicos = new Dictionary<string, int>();
-		public static string fileName = "";
 
 		public static void Main(String[] args) {
-			try {
-				IniciarMnemonicos();
-
-				Console.WriteLine("Raiz do arquivo .ula: ");
-				fileName = Console.ReadLine();
-				string[] str = fileName.Split("\\");
-				var inputFile = new StreamReader(new FileStream(fileName, FileMode.Open));
-
-				ArrayList listExpressions = LerArquivo(inputFile);
-
-				String outputFileName = fileName.Substring(0, fileName.LastIndexOf('.') + 1) + "hex";
-				CriarArquivo(listExpressions, outputFileName);
-				
-				inputFile.Close();
-			} catch (Exception e) {
-				Console.WriteLine(e.StackTrace);
-			}
-		}
-
-		public static ArrayList LerArquivo(StreamReader file) {
-			ArrayList hex = new ArrayList();
-			int a = 0;
-			int b = 0;
-			String line;
-
-			while (file.Peek() != -1) {
-				line = file.ReadLine();
-				line = line.Replace("\n", "").Replace(" ", "");
-				if (!(line.Equals("inicio:") || line.Equals("fim."))) {
-					char aux = line.ElementAt(0);
-					if (aux == 'X') {
-						char c = line.ElementAt(2);
-						a = c >= 48 && c <= 57 ? c - 48 : c - 55;
-					} else if (aux == 'Y') {
-						char c = line.ElementAt(2);
-						b = c >= 48 && c <= 57 ? c - 48 : c - 55;
-					} else {
-						string mnemonico = line.Substring(2, line.Length - 3);
-						string newString = new StringBuilder().Append(ParaHexa(a)).Append(ParaHexa(b)).Append(ParaHexa(mnemonicos[mnemonico])).ToString();
-						hex.Add(newString);
-					}
-					} else if (line.Equals("fim.")) {
-					break;
-				}
-			}
-
-			return hex;
-		}
-
-		public static void IniciarMnemonicos() {
 			mnemonicos.Add("An", 0);
 			mnemonicos.Add("nAoB", 1);
 			mnemonicos.Add("AnB", 2);
@@ -75,35 +24,57 @@ namespace AC2Project {
 			mnemonicos.Add("AoBn", 13);
 			mnemonicos.Add("AoB", 14);
 			mnemonicos.Add("copiaA", 15);
+
+			Console.WriteLine("Raiz do arquivo .ula: ");
+			string fileName = Console.ReadLine();
+			using (var inputFile = new StreamReader(new FileStream(fileName, FileMode.Open))) {
+				ArrayList hex = LerArquivo(inputFile);
+
+				String fileNameOut = fileName.Substring(0, fileName.LastIndexOf('.') + 1) + "hex";
+				CriarHex(hex, fileNameOut);
+			}
 		}
 
-		public static char ParaHexa(int value) {
+		public static ArrayList LerArquivo(StreamReader file) {
+			ArrayList hex = new ArrayList();
+			int a = 0, b = 0;
+
+			while (true) {
+				string line = file.ReadLine();
+				line = line.Replace("\n", "").Replace(" ", "");
+				if (line.Equals("fim.")) {
+					break;
+				}
+				if (!line.Equals("inicio:")) { 
+					char aux = line.ElementAt(0);
+					if (aux == 'X') {
+						char c = line.ElementAt(2);
+						a = c >= 48 && c <= 57 ? c - 48 : c - 55;
+					} else if (aux == 'Y') {
+						char c = line.ElementAt(2);
+						b = c >= 48 && c <= 57 ? c - 48 : c - 55;
+					} else {
+						string mnemonico = line.Substring(2, line.Length - 3);
+						string newString = "" + ParaHex(a) + ParaHex(b) + ParaHex(mnemonicos[mnemonico]).ToString();
+						hex.Add(newString);
+					}
+				}
+			}
+
+			return hex;
+		}
+
+		public static char ParaHex(int value) {
 			return (value < 10) ? value.ToString().ElementAt(0) : ((char)(value + 55));
 		}
 
-		public static void CriarArquivo(ArrayList result, String fileName) {
-			StreamWriter sw;
-			try {
-				sw = new StreamWriter(new FileStream(fileName, FileMode.OpenOrCreate));
-			} catch (Exception e) {
-				Console.WriteLine(e.StackTrace);
-				return;
-			}
-
-			try {
-				foreach (var expression in result) {
-					try {
-						sw.Write(expression + "\n");
-					} catch (Exception e) {
-						Console.WriteLine(e.StackTrace);
-					}
+		public static void CriarHex(ArrayList list, String fileName) {
+			using (StreamWriter sw = new StreamWriter(new FileStream(fileName, FileMode.OpenOrCreate))) {
+				foreach (var elem in list) {
+					sw.Write(elem + "\n");
 				}
 				sw.Write("fim ");
-			} catch (Exception e) {
-				Console.WriteLine(e.StackTrace);
 			}
-
-			sw.Close();
 		}
-	} 
+	}
 }
